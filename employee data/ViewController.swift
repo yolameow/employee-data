91ed6e4898f7292aac0657cc8884cd606c6ac311
Olga Yushkevich <olyal-ya@mail.ru>
2020-11-04 11:45:26 +0300

    Initial Commit

---
+++ employee data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  employee data
+//
+//  Created by Mac on 04.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
